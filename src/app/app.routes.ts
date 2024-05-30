e031ba082097090bb5506f8c21440738c250d36f
Carlosgrpl <cgonzalez.38@alumni.unav.es>
2024-05-30 21:48:04 +0200

    Angular: Routes and very basic home page for movies website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MovieFormPageComponent } from './pages/movie-form-page/movie-form-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /* Cada ruta será un objeto, que tiene dos propiedades, un path y un component */
+  {path: "", component: HomeComponent},
+  {path: "movies", component: MovieListComponent },
+  {path: "create", component: MovieFormPageComponent },
+];
+/* Al meterlo aqui debajo VSC lo mete arriba directamente */
---
+++ src/app/pages/movie-form-page/movie-form-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-movie-form-page',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './movie-form-page.component.html',
+  styleUrl: './movie-form-page.component.css'
+})
+export class MovieFormPageComponent {
+
+}
